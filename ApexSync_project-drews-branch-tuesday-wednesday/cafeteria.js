ecd76fe94d9bd60a39b83873db21fbc39fe07cb7
dreconner <112505310+dreconner@users.noreply.github.com>
2026-04-15 13:21:50 -0400

    Delete ApexSync_project-drews-branch-tuesday-wednesday directory

--- ApexSync_project-drews-branch-tuesday-wednesday/cafeteria.js
+++
@@ -1,7 +0,0 @@
-function placeOrder(itemName, selectId) {
-  const readyTime = document.getElementById(selectId).value;
-
-  alert(
-    `Your order for ${itemName} has been placed.\nIt will be ready in ${readyTime}.\nPlease pay when you pick it up.`
-  );
-}
